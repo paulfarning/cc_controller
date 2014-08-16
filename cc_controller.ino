53dbc855c0ee7f1745d7b28fadfeeaed84d2d89f
Paul Farning <paul@waytoocrowded.com>
2014-08-16 14:54:07 -0700

    Trigger serial port

--- cc_controller.ino
+++ cc_controller.ino
@@ -1,3 +1,7 @@
+#define HWSERIAL Serial1
+
+const int channel = 1;
+
 struct ccControl {
   int btnPin;
   int ledPin;
@@ -5,12 +9,13 @@ struct ccControl {
   int previous;
   int stateOn;
   int stateOff;
+  int cc;
   char name[10];
   int reading;
 };
 
-ccControl local = { 5, 21, HIGH, LOW, 127, 0, "local" };
-ccControl arp = { 6, 13, HIGH, LOW, 0, 127, "arp hold" };
+ccControl local = { 5, 21, HIGH, LOW, 127, 0, 122, "local" };
+ccControl arp = { 6, 13, HIGH, LOW, 0, 127, 117, "arp hold" };
 
 long time = 0;
 long debounce = 500;
@@ -18,38 +23,9 @@ long debounce = 500;
 char buffer[60];
 
 
-void initUI(struct ccControl control) {
-  pinMode(control.btnPin, INPUT_PULLUP);
-  pinMode(control.ledPin, OUTPUT);
-  sprintf(buffer, "Init MIDI value: %x", control.stateOn);
-  Serial.println(buffer);
-  digitalWrite(control.ledPin, control.previous);
-}
-
-
-void processButtonState(struct ccControl &control) {
-  if (control.reading == HIGH && control.previous == LOW && millis() - time > debounce) {
-    if (control.state == HIGH) {
-      sprintf(buffer, "Toggle %s led on: %x", control.name, control.stateOff);
-      Serial.println(buffer);
-    } else {
-      sprintf(buffer, "Toggle %s led off: %x", control.name, control.stateOn);
-      Serial.println(buffer);
-    }
-    digitalWrite(control.ledPin, control.state);
-
-    if (control.state == HIGH)
-      control.state = LOW;
-    else
-      control.state = HIGH;
-
-    time = millis();
-  }
-  control.previous = control.reading;
-}
-
-
 void setup() {
+  Serial.begin(31250);
+  HWSERIAL.begin(31250);
   initUI(local);
   initUI(arp);
 }
@@ -63,4 +39,49 @@ void loop() {
   arp.reading = digitalRead(arp.btnPin);
   processButtonState(arp);
 
+}
+
+
+void initUI(struct ccControl control) {
+  pinMode(control.btnPin, INPUT_PULLUP);
+  pinMode(control.ledPin, OUTPUT);
+  sprintf(buffer, "Init MIDI value: %x", control.stateOn);
+  Serial.println(buffer);
+  digitalWrite(control.ledPin, control.previous);
+}
+
+
+void sendCC(int cmd, int data1, int data2) {
+  HWSERIAL.write(cmd);
+  HWSERIAL.write(data1);
+  HWSERIAL.write(data2);
+  Serial.println(cmd, DEC);
+  Serial.println(data1, DEC);
+  Serial.println(data2, DEC);
+}
+
+
+void processButtonState(struct ccControl &control) {
+  if (control.reading == HIGH && control.previous == LOW && millis() - time > debounce) {
+    if (control.state == HIGH) {
+      usbMIDI.sendControlChange(control.cc, control.stateOff, channel);
+      sendCC(control.cc, control.stateOff, control.stateOff);
+      sprintf(buffer, "Toggle %s led on: %x", control.name, control.stateOff);
+      Serial.println(buffer);
+    } else {
+      usbMIDI.sendControlChange(control.cc, control.stateOn, channel);
+      sendCC(control.cc, control.stateOn, control.stateOn);
+      sprintf(buffer, "Toggle %s led off: %x", control.name, control.stateOn);
+      Serial.println(buffer);
+    }
+    digitalWrite(control.ledPin, control.state);
+
+    if (control.state == HIGH)
+      control.state = LOW;
+    else
+      control.state = HIGH;
+
+    time = millis();
+  }
+  control.previous = control.reading;
 }
