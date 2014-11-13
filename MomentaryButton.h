1bb52a140f08183a92388c577bb13717ae692bda
Paul Farning <paul@waytoocrowded.com>
2014-11-12 18:59:50 -0800

    Start multimode button press.

---
+++ MomentaryButton.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+// Any button press >= HOLD_THRESHOLD milliseconds is considered a hold,
+//  not a click.
+#define HOLD_THRESHOLD   2000
+
+/**
+ * Encapsulate tracking pushes on a normally-open momentary button.
+ *
+ * After a call to check(), the button may report that it:
+ *  - wasClicked(): tapped briefly
+ *  - wasHeld(): pressed for a longer period of time (and perhaps still closed)
+ * The next call to check() will not report on that previous event.
+ */
+class MomentaryButton {
+private:
+  // Was this button pushed (closed) when last checked?
+  boolean wasClosed;
+  // Only report a hold once (even though the condition
+  //  is satisfied continuously).
+  boolean holdReported;
+  // When was this button pushed (closed)?
+  unsigned long closeTimeMillis;
+  const int pin;
+  boolean clicked, held; // At last check, was this button clicked (held)?
+public:
+  MomentaryButton(int inputPin);
+
+  void setup();
+
+  void check();
+
+  boolean wasClicked();
+
+  boolean wasHeld();
+
+  boolean wasReleased();
+};
--- cc_controller.ino
+++ cc_controller.ino
@@ -23,10 +23,12 @@
 #include <SevSeg.h>
 #include "CcButton.h"
 #include "CcEncoder.h"
+#include "MomentaryButton.h"
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
 const int debounceMS = 500;
+const int holdMS = 2000;
 const int sendEncodersBtnPin = 2;
 const int initDelay = 4000;
 
@@ -48,7 +50,23 @@ CcEncoder CcEncoders[] = {
   CcEncoder(31, 30, 29, 127, 0, debounceMS, "CC Value") // Right encoder
 };
 
-Bounce sendEncodersBtn = Bounce(sendEncodersBtnPin, debounceMS);
+// Bounce sendEncodersBtn = Bounce(sendEncodersBtnPin, debounceMS);
+
+// MomentaryButton button(sendEncodersBtnPin);
+
+// int buttonVal;
+// int btnUpTime;
+// int buttonLast;
+// int ignoreUp;
+// unsigned long btnDnTime;
+
+
+
+unsigned long keyPrevMillis = 0;
+const unsigned long keySampleIntervalMs = 25;
+byte longKeyPressCountMax = 80;    // 80 * 25 = 2000 ms
+byte longKeyPressCount = 0;
+byte prevKeyState = HIGH;         // button is active low
 
 
 /**
@@ -66,6 +84,8 @@ void setup() {
   for(int i = 0; i < ARRAY_SIZE(CcEncoders); i++) {
     CcEncoders[i].begin();
   }
+
+  // button.setup();
 
   setupDisplay();
 
@@ -85,20 +105,87 @@ void loop() {
   }
 
   // Send encoders if button pushed.
-  if (sendEncodersBtn.update()) {
-    if (sendEncodersBtn.risingEdge()) {
-      usbMIDI.sendControlChange(
-        CcEncoders[1].read(),
-        CcEncoders[0].read(),
-        midiChannel
-      );
-      MIDI.sendControlChange(
-        CcEncoders[1].read(),
-        CcEncoders[0].read(),
-        midiChannel
-      );
+  // if (sendEncodersBtn.update()) {
+  //   if (sendEncodersBtn.fallingEdge()) {
+  //     Serial.println("clicked");
+  //     usbMIDI.sendControlChange(
+  //       CcEncoders[1].read(),
+  //       CcEncoders[0].read(),
+  //       midiChannel
+  //     );
+  //     MIDI.sendControlChange(
+  //       CcEncoders[1].read(),
+  //       CcEncoders[0].read(),
+  //       midiChannel
+  //     );
+  //   }
+  // }
+
+  // button.check();
+  // if (button.wasClicked()) {
+  //   Serial.println("clicked");
+  // } else if (button.wasHeld()) {
+  //   Serial.println("held");
+  // }
+
+  // if (sendEncodersBtn.update()) {
+  //   button.check();
+  //   if (button.wasClicked()) {
+  //     if (sendEncodersBtn.fallingEdge()) {
+  //       Serial.println("clicked");
+  //       usbMIDI.sendControlChange(
+  //         CcEncoders[1].read(),
+  //         CcEncoders[0].read(),
+  //         midiChannel
+  //       );
+  //       MIDI.sendControlChange(
+  //         CcEncoders[1].read(),
+  //         CcEncoders[0].read(),
+  //         midiChannel
+  //       );
+  //     }
+  //   } else if (button.wasHeld()) {
+  //    if (sendEncodersBtn.risingEdge()) {
+  //       Serial.println("held");
+
+  //       usbMIDI.sendControlChange(
+  //         CcEncoders[1].read(),
+  //         CcEncoders[0].read(),
+  //         midiChannel
+  //       );
+  //       MIDI.sendControlChange(
+  //         CcEncoders[1].read(),
+  //         CcEncoders[0].read(),
+  //         midiChannel
+  //       );
+  //     }
+  //   }
+  // }
+
+  // checkEncodersButton();
+
+
+    if (millis() - keyPrevMillis >= keySampleIntervalMs) {
+        keyPrevMillis = millis();
+
+        byte currKeyState = digitalRead(sendEncodersBtnPin);
+
+        if ((prevKeyState == HIGH) && (currKeyState == LOW)) {
+            keyPress();
+        }
+        else if ((prevKeyState == LOW) && (currKeyState == HIGH)) {
+            keyRelease();
+        }
+        else if (currKeyState == LOW) {
+            longKeyPressCount++;
+        }
+
+        prevKeyState = currKeyState;
     }
-  }
+
+
+
+
 
   // Update CC buttons.
   for (int i = 0; i < ARRAY_SIZE(CcButtons); i++) {
@@ -192,3 +279,90 @@ int getEncoderToDisplay() {
   }
   return encoderIndex;
 }
+
+// void checkEncodersButton() {
+//   // Read the state of the button
+//   buttonVal = digitalRead(sendEncodersBtnPin);
+
+//   // // Test for button pressed and store the down time
+//   if (buttonVal == LOW && buttonLast == HIGH && (millis() - btnUpTime) > long(debounceMS)) {
+//     btnDnTime = millis();
+
+//     Serial.println(btnDnTime);
+//   }
+
+//   // // Test for button release and store the up time
+
+//   if (buttonVal == HIGH && buttonLast == LOW && (millis() - btnDnTime) > long(debounceMS)) {
+//     Serial.println("here");
+//     if (ignoreUp == false) {
+
+//     Serial.println("in");
+//       event1();
+//     } else {
+//       ignoreUp = false;
+
+//     Serial.println("out");
+//     }
+//     btnUpTime = millis();
+
+//     Serial.println("done");
+//   }
+
+//   // // Test for button held down for longer than the hold time
+//   if (buttonVal == LOW && (millis() - btnDnTime) > long(holdMS)) {
+//     event2();
+//     ignoreUp = true;
+//     btnDnTime = millis();
+//   }
+
+//   buttonLast = buttonVal;
+// }
+
+
+// void event1() {
+//   Serial.println("clicked");
+// }
+
+// void event2() {
+//   Serial.println("held");
+// }
+//
+//
+//
+//
+
+
+
+// called when button is kept pressed for less than 2 seconds
+void shortKeyPress() {
+    Serial.println("short");
+}
+
+
+// called when button is kept pressed for more than 2 seconds
+void longKeyPress() {
+    Serial.println("long");
+}
+
+
+// called when key goes from not pressed to pressed
+void keyPress() {
+    Serial.println("key press");
+    longKeyPressCount = 0;
+}
+
+
+// called when key goes from pressed to not pressed
+void keyRelease() {
+    Serial.println("key release");
+
+    if (longKeyPressCount >= longKeyPressCountMax) {
+        longKeyPress();
+    }
+    else {
+        shortKeyPress();
+    }
+}
+
+
